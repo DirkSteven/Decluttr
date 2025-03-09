ab8fa3b98907ab1f608aa70f74f6c805197acd3d
Sherivic Mae Dimagculang <123381697+sherivicmae@users.noreply.github.com>
2025-03-09 17:33:06 +0800

    Update models.py

--- decluttr/models.py
+++ decluttr/models.py
@@ -3,6 +3,15 @@ from decluttr import db
 from flask_login import UserMixin
 from sqlalchemy import Numeric
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    user_level = db.Column (db.Integer, db.ForeignKey('user_level.user_level'), default=1, nullable=True)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}', '{self.sr_code}', '{self.program}')"
 
     
 class Cart(db.Model):
